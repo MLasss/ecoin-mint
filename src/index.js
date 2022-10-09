026cbda588c7da6a235fc3e9c9cb3407cac54b40
unknown <MarcusLaska@protonmail.com>
2022-10-09 12:44:45 +0200

    style update

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { HashRouter } from "react-router-dom";
-
-import './assets/css/base.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.min.js'
 import "./assets/css/base.css"
