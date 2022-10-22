283a30caed93f542f4d3d03fb4e038bb3896f072
unknown <MarcusLaska@protonmail.com>
2022-10-22 14:39:51 +0200

    exception handlnig on lib

--- src/lib/datastorage.js
+++ src/lib/datastorage.js
@@ -1,11 +1,9 @@
 import { ethers } from "ethers";
-
 import abi from "../contracts/dataStorage.json";
 
-const polygonProvider = new ethers.providers.Web3Provider(window.ethereum);
-const contract = new ethers.Contract(process.env.REACT_APP_DATASTORE_ADDRESS, abi , polygonProvider)
-
 export async function getTemplate(templateId){
+    const polygonProvider = new ethers.providers.Web3Provider(window.ethereum);
+    const contract = new ethers.Contract(process.env.REACT_APP_DATASTORE_ADDRESS, abi, polygonProvider)
     const data = await contract.getTemplate(templateId);
     return data;
 }
--- src/lib/userAccess.js
+++ src/lib/userAccess.js
@@ -1,18 +1,18 @@
 import { ethers } from "ethers";
-
 import abi from "../contracts/userAccess.json";
 
-const polygonProvider = new ethers.providers.Web3Provider(window.ethereum);
-const signer = polygonProvider.getSigner();
-const contract = new ethers.Contract(process.env.REACT_APP_USERACCESS_ADDRESS, abi, signer)
-
 export async function getMintPrice(templateId){
+    const polygonProvider = new ethers.providers.Web3Provider(window.ethereum);
+    const contract = new ethers.Contract(process.env.REACT_APP_USERACCESS_ADDRESS, abi, polygonProvider)
     const data = await contract.getMintPrice(templateId);
     return data;
 }
 
 export async function mintCoin(templateId){
     try {
+        const polygonProvider = new ethers.providers.Web3Provider(window.ethereum);
+        const signer = polygonProvider.getSigner();
+        const contract = new ethers.Contract(process.env.REACT_APP_USERACCESS_ADDRESS, abi, signer)
         const data = await contract.mintCoin(templateId);
         await polygonProvider.waitForTransaction(data.hash);
         return '1';
