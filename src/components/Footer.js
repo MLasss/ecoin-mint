133397494248aafeb79ffaa7bbdfef4b8357aa84
unknown <MarcusLaska@protonmail.com>
2023-01-30 18:11:25 +0100

    enable minting

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,20 @@
+import { useState, useEffect } from 'react';
 import "../assets/css/base.css";
 
 function Footer() {
+    const [currentYear, setCurrentYear] = useState('');
+
+    useEffect(() => {
+      setCurrentYear(new Date().getFullYear());
+    }, []);
+
   return (
     <>
     <div>
       <footer className="py-1 bg-dark fixed-bottom footer">
         <div className="container">
           <p className="m-0 text-center text-white">
-            &copy; 2021 - 2022 EmojiCoins
+            &copy; 2021 - {currentYear} <a href="http://emojicoins.org/">emojicoins.org</a>
           </p>
         </div>
       </footer>
